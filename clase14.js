a763fb78ff4c72d1098eef8fe3c8fea8c2291d71
Ana Maria <anamariamongui@gmail.com>
2020-05-29 23:44:11 -0500

    Cambios Clase 14 a Clase 17

--- clase14.js
+++ clase14.js
@@ -7,20 +7,25 @@ var Ana = {
 
 console.log(`Al inicio del año ${Ana.nombre} pesa ${Ana.peso} kg`)
 
-const INCREMENTO_PESO = 0.2
+const INCREMENTO_PESO = 0.3
 const DIAS_DEL_ANO = 365
 
 const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
 const adelgazar = persona => persona.peso -= INCREMENTO_PESO
+const comeMucho = () => Math.random() < 0.3
+const realizaDeporte = () => Math.random() < 0.4
 
-for (var i = 1; i <= DIAS_DEL_ANO; i++) {
-    var random = Math.random()
+const META = Ana.peso -3 
+var dias = 0
 
-    if (random < 0.25) {
+while(Ana.peso > META){
+    if (comeMucho()){
         aumentarDePeso(Ana)
-    } else if (random < 0.5) {
+    }
+    if(realizaDeporte()){
         adelgazar(Ana)
     }
+    dias += 1
 }
 
-console.log(`Al Final del año ${Ana.nombre} pesa ${Ana.peso.toFixed(1)} kg`)
+console.log(`Pasaron ${dias} dias hasta que ${Ana.nombre} adelgazo 3 kg`)
---
+++ clase16.js
@@ -0,0 +1,46 @@
+var signo = prompt (' ¿Cuál es tu signo?')
+
+switch (signo){
+    case 'aries':
+        console.log(' Debería comenzar a quererse más en la vida. Durante esta jornada, tendrá que empezar a amarse a usted mismo sin esperar nada a cambio de los demás. ')
+        break
+    case 'tauro':     
+        console.log(' Si no obtuvo los resultados esperados en ese proyecto, será el momento de poner en acción las diferentes alternativas para lograr dicho propósito. ')
+        break
+    case 'geminis':
+    case 'géminis':        
+        console.log(' Prepárese, ya que su ritmo de vida social y el exceso de trabajo lo sumergirán en un mundo material que lo mantendrá insatisfecho. Busque algún pasatiempo. ')
+        break
+    case 'cancer':
+        console.log(' De una vez por todas, anímese y aprenda a disfrutar de la vida. Trate de no olvidarse de sus propios deseos y deje de postergar más tiempo para cumplirlos. ')
+        break
+    case 'leo':
+        console.log(' No desaproveche esta nueva etapa que le brinda la vida, para reafirmar su seguridad y el temperamento que lo caracteriza. Ponga todo de usted. ')
+        break
+    case 'virgo':
+        console.log(' No es un buen momento para renunciar a sus propósitos, así logrará el éxito rotundo. Llego el día en el que se sentirá consolidado y estimulado para seguir adelante. ')
+        break
+    case 'libra':
+        console.log(' Jamás debe dar un sí apresurado. Durante esta jornada, se le presentarán decisiones que no podrá tomar en un solo día y tendrá que ser cuidadoso. ')
+        break
+    case 'escorpion':    
+        console.log(' Relájese, ya que su visión lo ayudará a descubrir la verdad que usted no ve a simple vista. Desconfíe de aquellos que repentinamente le ofrecen todo tipo de ayuda. ')
+        break
+    case 'sagitario':    
+        console.log(' Procure medir sus reacciones emocionales. Debería evitar las discusiones con la gente que lo rodea, caso contrario, luego lamentará las consecuencias. ')
+        break
+    case 'capricornio':
+        console.log(' No pierda tiempo en cosas que no valen la pena y empiece a pensar en su futuro. Aproveche esta nueva etapa para innovar y cambiar de rumbo su vida cotidiana. ')
+        break
+    case 'acuario':   
+        console.log(' Debería dejar atrás todo aquello que es viejo y le hace daño recordar. Ilumine su interior enfocando su mente en cosas positivas y pronto verá que todo mejorará. ')
+        break
+    case 'piscis':    
+        console.log(' Procure manejar las cuestiones profesionales con mayor racionalidad en la vida. Sepa que necesitará ser más sensato en el análisis de su situación laboral. ')
+        break
+    default:
+        console.log( 'No se encontro el signo que indica')
+        break
+
+
+} 
